ac7e4930279a0efb8deea9779cd2e02718ccd9e9
Komron Mukhamadulloev <kmukhamadulloev@gmail.com>
2022-01-15 13:42:24 +0500

    sync

---
+++ table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `contacts` (
+    `id` INT AUTO_INCREMENT NOT NULL,
+    `first_name` VARCHAR(150) NOT NULL,
+    `last_name` VARCHAR(150) NOT NULL,
+    `phone` VARCHAR(20) NOT NULL,
+    `comments` VARCHAR(200),
+    PRIMARY KEY(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=uf8;
+
+INSERT INTO contacts(first_name, last_name, phone, comments) VALUES
+('Komron', 'Mukhamadulloev', '992931668172', 'no data'),
+('Andrey', 'Pavliashvili', '78931668172', 'some armenian guy who tried to sell me some stuff'),
+('Jim', 'Carr', '245931668172', 'funny guy, comedian'),
+('Zokir', 'Satorov', '992988586421', 'guy who can fix your router');
